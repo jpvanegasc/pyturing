b85fbe7335ce2bc913e20c607a700ea6c6cf0be8
Juan Vanegas <juan.vanegas@loka.com>
2025-01-18 22:13:41 -0500

    :bug: Add halting state correctly

--- examples/addition.py
+++ examples/addition.py
@@ -13,11 +13,12 @@ addition = {
         0: ("q3", 0, 1),
         1: ("q2", 0, -1),
     },
-    "q3": {1: ("q3", 1, 1)},
+    "q3": {1: ("H", 1, 1)},
+    "H": {0: ("H", 0, 0), 1: ("H", 1, 0)},
 }
 
 
-tape = [0, 1, 1, 0, 1, 1, 1]
-transition = TransitionFunction(addition, halt_states=["q3"])
+tape = [0, 1, 1, 1, 0, 1, 1, 1]
+transition = TransitionFunction(addition, halt_states=["H"])
 tm = TuringMachine(tape, transition)
 tm.run()
